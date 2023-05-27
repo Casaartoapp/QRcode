969d22139dc563f1da5e984bf698671afd5084d2
Casaartoapp <134726379+Casaartoapp@users.noreply.github.com>
2023-05-27 10:07:36 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function GerarQRCode(){
+    var inputUuario = document.querySelector('texarea').value;
+    var GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=500x500&chld=H&chl=';
+    var conteudoQRCODE = GoogleChartAPI + encodeURIComponent(inputUsuario);
+    document.querySelector('#QRCodeImage').src = conteudoQRCode 
+}
